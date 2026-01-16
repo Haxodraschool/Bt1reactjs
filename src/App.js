dde81eafa75351c359576ad4e85871e9f95abfb5
Haxodras <haxodras@gmail.com>
2026-01-16 14:56:57 +0700

    2nd

--- src/App.js
+++ src/App.js
@@ -6,32 +6,52 @@ import siri from './img/siri.png';
 
 function App() {
   return (
-    <div className="container">
-      <div className="card">
-        <img src={alexa} alt="Alexa" />
-        <div className="card-content">
-          <h3>Alexa</h3>
-          <span className="username">@alexa99</span>
-          <p>I'll help you buy stuff off Amazon</p>
+    <section className="section">
+      <div className="columns is-centered is-multiline">
+        <div className="column is-narrow">
+          <div className="card custom-card">
+            <div className="card-image">
+              <figure className="image">
+                <img src={alexa} alt="Alexa" />
+              </figure>
+            </div>
+            <div className="card-content">
+              <p className="title is-5 mb-1">Alexa</p>
+              <p className="username has-text-info is-size-7 mb-2">@alexa99</p>
+              <p className="is-size-7">I'll help you buy stuff off Amazon</p>
+            </div>
+          </div>
+        </div>
+        <div className="column is-narrow">
+          <div className="card custom-card">
+            <div className="card-image">
+              <figure className="image">
+                <img src={cortana} alt="Cortana" />
+              </figure>
+            </div>
+            <div className="card-content">
+              <p className="title is-5 mb-1">Cortana</p>
+              <p className="username has-text-info is-size-7 mb-2">@cortana12</p>
+              <p className="is-size-7">Personal assistant by Microsoft</p>
+            </div>
+          </div>
+        </div>
+        <div className="column is-narrow">
+          <div className="card custom-card">
+            <div className="card-image">
+              <figure className="image">
+                <img src={siri} alt="Siri" />
+              </figure>
+            </div>
+            <div className="card-content">
+              <p className="title is-5 mb-1">Siri</p>
+              <p className="username has-text-info is-size-7 mb-2">@siri44</p>
+              <p className="is-size-7">I don't get a lot of updates anymore</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="card">
-        <img src={cortana} alt="Cortana" />
-        <div className="card-content">
-          <h3>Cortana</h3>
-          <span className="username">@cortana12</span>
-          <p>Personal assistant by Microsoft</p>
-        </div>
-      </div>
-      <div className="card">
-        <img src={siri} alt="Siri" />
-        <div className="card-content">
-          <h3>Siri</h3>
-          <span className="username">@siri44</span>
-          <p>I don't get a lot of updates anymore</p>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 }
 

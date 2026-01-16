be581147270779f9cd915152a87a7c8dfc05e472
Haxodras <haxodras@gmail.com>
2026-01-16 14:25:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import alexa from './img/alexa.png';
+import cortana from './img/cortana.png';
+import siri from './img/siri.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <img src={alexa} alt="Alexa" />
+        <div className="card-content">
+          <h3>Alexa</h3>
+          <span className="username">@alexa99</span>
+          <p>I'll help you buy stuff off Amazon</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={cortana} alt="Cortana" />
+        <div className="card-content">
+          <h3>Cortana</h3>
+          <span className="username">@cortana12</span>
+          <p>Personal assistant by Microsoft</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={siri} alt="Siri" />
+        <div className="card-content">
+          <h3>Siri</h3>
+          <span className="username">@siri44</span>
+          <p>I don't get a lot of updates anymore</p>
+        </div>
+      </div>
     </div>
   );
 }
